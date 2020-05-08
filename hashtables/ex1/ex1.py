cca6b1f2741e5640657da44ebef708af548caa88
Dorothy Gasque <dggasque@gmail.com>
2020-05-08 12:35:35 -0700

    Update ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -10,11 +10,12 @@ def get_indices_of_item_weights(weights, length, limit):
         else:
             dic[x] = [i]   
     for key1 in dic:
+        key2 = limit - key1 
         if len(dic[key1]) > 1:
             if key1 * 2 == limit:
                 return (dic[key1][1], dic[key1][0])
-        for key2 in dic:
-            if key1 != key2 and key2 == limit-key1:
+        if key2 in dic:
+            if key1 != key2:
                 index1 = dic[key1][0]
                 index2 = dic[key2][0]
                 if index1 < index2:
